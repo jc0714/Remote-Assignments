4a62163431e843f161deb6dc769f31174af98cac
Joyce <0714joyce@gmail.com>
2024-06-27 13:44:27 +0800

    作業第五題

--- Week2/Story/Story/ViewController.swift
+++ Week2/Story/Story/ViewController.swift
@@ -4,17 +4,18 @@
 //
 //  Created by J oyce on 2024/6/25.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var funFactLabel: UILabel!
-
+    @IBOutlet weak var showButton: UIButton! //想要按鈕上的文字一起變色，但研究了好幾個小時他都沒有變 傷心
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 }
+
 let funFacts = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
     "Contrary to popular belief, Lorem Ipsum is not simply random text.",
@@ -29,21 +30,24 @@ extension ViewController {
         // 用 random index 隨機出數字
         let randomIndex = Int.random(in: 0..<funFacts.count)
         let randomFact = funFacts[randomIndex]
-        //print(randomFact)
+        
         funFactLabel.text = randomFact
-      
-        view.backgroundColor = getRandomColor()
+        
+        let randomColor = getRandomColor()
+        view.backgroundColor = randomColor
+        
+        sender.setTitleColor(randomColor, for: .normal) //這句好像沒用？
         
     }
-    //RGB + random 隨機浮點數創造隨機 RGB 值
+    
+    //RGB + random 隨機浮點數創造隨機顏色
     func getRandomColor() -> UIColor {
         let red = CGFloat.random(in: 0...1)
         let green = CGFloat.random(in: 0...1)
-        let blue = CGFloat.random(in: 0...1)
-        return UIColor(red: red, green: green, blue: blue, alpha: 1)//透明度
+        let blue = CGFloat.random(in: 0...0.8)
+        return UIColor(red: red, green: green, blue: blue, alpha: 1) // 透明度
     }
 }
-
 
 
 
